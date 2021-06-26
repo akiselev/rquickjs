9a083b34a04cdcf37e2ea14b97c9416e00b504de
Alexander Kiselev <git@akiselev.com>
2021-06-26 13:08:56 -0700

    to_csttring

--- rquickjs-core/src/value/string.rs
+++ rquickjs-core/src/value/string.rs
@@ -1,5 +1,8 @@
 use crate::{handle_exception, qjs, Ctx, Error, Result, StdString, Value};
-use std::{mem, slice, str};
+use std::{
+    ffi::{CStr, CString},
+    mem, slice, str,
+};
 
 /// Rust representation of a javascript string.
 #[derive(Debug, Clone, PartialEq)]
@@ -7,6 +10,17 @@ use std::{mem, slice, str};
 pub struct String<'js>(pub(crate) Value<'js>);
 
 impl<'js> String<'js> {
+    pub fn to_cstring(&self) -> Result<CString> {
+        let ptr = unsafe { qjs::JS_ToCString(self.0.ctx.ctx, self.0.as_js_value()) };
+        if ptr.is_null() {
+            // Might not ever happen but I am not 100% sure
+            // so just incase check it.
+            return Err(Error::Unknown);
+        }
+        let result = unsafe { CString::from_raw(ptr as *mut i8) };
+        Ok(result)
+    }
+
     /// Convert the javascript string to a rust string.
     pub fn to_string(&self) -> Result<StdString> {
         let mut len = mem::MaybeUninit::uninit();
